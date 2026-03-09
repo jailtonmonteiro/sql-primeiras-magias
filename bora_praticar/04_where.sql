13137287ef4313380b6575c546e1def0b53fa94b
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2026-03-09 17:19:01 -0300

    feat: Bora praticar 01

---
+++ bora_praticar/04_where.sql
@@ -0,0 +1,16 @@
+-- Selecione produtos que contêm ‘churn’ no nome
+
+SELECT *
+
+FROM produtos
+
+-- Churn_10pp
+-- Churn_2pp
+-- Churn_5pp
+
+-- WHERE DescNomeProduto = 'Churn_10pp'
+-- OR DescNomeProduto = 'Churn_2pp'
+
+-- WHERE DescNomeProduto IN ('Churn_10pp', 'Churn_2pp', 'Churn_5pp')
+
+WHERE DescNomeProduto LIKE 'churn%'
