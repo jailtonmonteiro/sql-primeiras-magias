c5b87c79cb01dde099e6a26b7f953ebd8012e627
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2026-04-02 16:11:35 -0300

    feat: Agrupando informações.

---
+++ 12_groupby.sql
@@ -0,0 +1,21 @@
+/* SELECT  IdProduto,
+        count(*)
+
+FROM transacao_produto
+
+GROUP BY IdProduto */
+
+SELECT  idCliente,
+        sum(QtdePontos) AS saldoCliente,
+        count(IdTransacao) AS QtdeTransacao
+
+FROM transacoes
+
+WHERE DtCriacao > '2025-07-01'
+AND DtCriacao < '2025-08-01'
+
+GROUP BY idCliente
+
+ORDER BY saldoCLiente DESC
+
+LIMIT 10
