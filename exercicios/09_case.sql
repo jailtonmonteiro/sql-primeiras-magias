e00bddcca771ebf79e559f5cb9b65ace39220fc4
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2026-04-07 15:03:35 -0300

    feat: adiciona descrição no exercício

--- exercicios/09_case.sql
+++ exercicios/09_case.sql
@@ -1,3 +1,5 @@
+-- Listar todas as transações adicionando uma coluna nova sinalizando “alto”, “médio” e “baixo” para o valor dos pontos [<10 ; <500; >=500]
+
 SELECT IdTransacao,
        qtdePontos,
 
