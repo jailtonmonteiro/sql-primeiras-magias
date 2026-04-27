badd22d8dea55fdd4349331043bf861b071a4b01
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2026-04-27 16:31:22 -0300

    fix: Otmização de consulta.

--- exercicios/02_join.sql
+++ exercicios/02_join.sql
@@ -1,33 +1,29 @@
 -- Quais clientes assinaram a lista de presença no dia 2025/08/25
 -- Lista de presença: 11
 
-SELECT  t1.idCliente,
-        t2.DtCriacao
-
-FROM clientes t1
-
-LEFT JOIN transacoes t2
-ON t1.idCliente = t2.idCliente
-
-LEFT JOIN transacao_produto t3
-ON t2.IdTransacao = t3.IdTransacao
-
-WHERE substr(t2.DtCriacao, 1, 10) = '2025-08-25'
-AND t3.IdProduto = '11'
-
 /* SELECT  t1.idCliente,
-        t2.DtCriacao
+        t1.DtCriacao
 
-FROM clientes t1
+FROM transacoes t1
 
-LEFT JOIN transacoes t2
-ON t1.idCliente = t2.idCliente
+LEFT JOIN transacao_produto t2
+ON t1.IdTransacao = t2.IdTransacao
 
-LEFT JOIN transacao_produto t3
-ON t2.IdTransacao = t3.IdTransacao
+WHERE substr(t1.DtCriacao, 1, 10) = '2025-08-25'
+AND t2.IdProduto = '11'
 
-LEFT JOIN produtos t4
-ON t3.IdProduto = t4.IdProduto
+GROUP BY idCliente */
 
-WHERE substr(t2.DtCriacao, 1, 10) = '2025-08-25'
-AND t4.DescNomeProduto = 'Lista de presença' */
+SELECT  t1.idCliente,
+        t1.DtCriacao
+
+FROM transacoes t1
+
+LEFT JOIN transacao_produto t2
+ON t1.IdTransacao = t2.IdTransacao
+
+LEFT JOIN produtos t3
+ON t2.IdProduto = t3.IdProduto
+
+WHERE substr(t1.DtCriacao, 1, 10) = '2025-08-25'
+AND t3.DescNomeProduto = 'Lista de presença'
