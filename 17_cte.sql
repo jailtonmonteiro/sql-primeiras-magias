f932ff291283dff352e195e7dcc4f87327db1204
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2026-05-05 15:01:21 -0300

    feat: Aula cte completa.

--- 17_cte.sql
+++ 17_cte.sql
@@ -26,7 +26,19 @@ tb_cliente_ultimo_dia AS (
     SELECT DISTINCT idCliente
     FROM transacoes
     WHERE substr(DtCriacao, 1, 10) = '2025-08-29'
+),
+
+tb_join AS (
+
+    SELECT t1.idCliente AS primCliente, t2.idCliente AS ultCliente
+    FROM tb_cliente_primeiro_dia AS t1
+    LEFT JOIN tb_cliente_ultimo_dia AS t2
+    ON t1.idCliente = t2.idCliente
 )
 
-SELECT * 
-FROM tb_cliente_ultimo_dia
+SELECT  count(primCliente),
+        count(ultCliente),
+        1. * count(ultCliente) / count(primCliente)
+FROM tb_join
+
+
