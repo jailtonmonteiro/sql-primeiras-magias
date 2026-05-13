b8c25016ddfde4f5c2d5bc84dd10b8fa5e4dbfa6
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2026-05-13 14:27:37 -0300

    fix: window function finalização de aulas.

--- 20_window_function.sql
+++ 20_window_function.sql
@@ -1,3 +1,28 @@
-SELECT  *
+WITH cliente_dia AS (
+    SELECT  DISTINCT
+            idCliente,
+            substr(DtCriacao, 1, 10) AS dtDia
+    FROM transacoes
+    WHERE substr(DtCriacao, 1, 4) = '2025'
+    ORDER BY idCliente, dtDia
+),
+tb_lag_dia AS (
 
-FROM transacoes
+    SELECT  *,
+            lag(dtDia) OVER (PARTITION BY idCliente ORDER BY dtDia) AS lagDia
+    FROM cliente_dia
+),
+tb_diff_dt AS (
+    SELECT  *,
+            julianday(dtDia) - julianday(lagDia) AS dtDiff
+    FROM tb_lag_dia
+),
+avg_cliente AS (
+    SELECT  idCliente,
+            avg(dtDiff) AS avgDia
+    FROM tb_diff_dt
+    GROUP BY idCliente
+)
+
+SELECT avg(avgDia) AS avgGeral
+FROM avg_cliente
